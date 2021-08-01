7ee7e702ff4303430cfcff6861755dd8e8891191
Adilius <leugue-gamer@hotmail.com>
2021-08-01 12:56:37 +0200

    Weekly supply drop request header cleanup

--- AdiliusWSDG.py
+++ AdiliusWSDG.py
@@ -90,20 +90,7 @@ def weekly_supply_drop_request(session, WEBHALLEN_USER_ID: str):
     URL = 'https://www.webhallen.com/api/supply-drop'
 
     headers = {
-        'authority': 'www.webhallen.com',
-        'sec-ch-ua': '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
-        'dnt': '1',
-        'sec-ch-ua-mobile': '?0',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'accept': '*/*',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-dest': 'empty',
-        'origin': 'https://www.webhallen.com',
-        'referer': 'https://www.webhallen.com/se/member/'+str(WEBHALLEN_USER_ID)+'/supply-drop',
-        'accept-language': 'sv-SE,sv;q=0.9,en-US;q=0.8,en;q=0.7',
-        'content-length': '0',
-        'region': 'se'
+        'referer': 'https://www.webhallen.com/se/member/'+str(WEBHALLEN_USER_ID)+'/supply-drop'
     }
 
     print('Sending request to grab weekly supply drop...')
